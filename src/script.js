ab4d21e71c8799ff34ef8f75c72d3d3045331710
Chase Malik <chase.rs.malik@gmail.com>
2022-01-08 01:59:06 -0600

    Format

--- src/script.js
+++ src/script.js
@@ -1,76 +1,95 @@
 const digits = [
-[// 0
-{ hour: 6, minute: 15 },
-{ hour: 9, minute: 30 },
-{ hour: 6, minute: 0 },
-{ hour: 0, minute: 30 },
-{ hour: 3, minute: 0 },
-{ hour: 0, minute: 45 }],
-[// 1
-{ hour: 7.5, minute: 37.5 },
-{ hour: 6, minute: 30 },
-{ hour: 7.5, minute: 37.5 },
-{ hour: 6, minute: 0 },
-{ hour: 7.5, minute: 37.5 },
-{ hour: 0, minute: 0 }],
-[// 2
-{ hour: 3, minute: 15 },
-{ hour: 9, minute: 30 },
-{ hour: 6, minute: 15 },
-{ hour: 0, minute: 45 },
-{ hour: 0, minute: 15 },
-{ hour: 9, minute: 45 }],
-[// 3
-{ hour: 3, minute: 15 },
-{ hour: 9, minute: 30 },
-{ hour: 3, minute: 15 },
-{ hour: 9, minute: 0 },
-{ hour: 3, minute: 15 },
-{ hour: 9, minute: 0 }],
-[// 4
-{ hour: 6, minute: 30 },
-{ hour: 6, minute: 30 },
-{ hour: 0, minute: 15 },
-{ hour: 6, minute: 0 },
-{ hour: 7.5, minute: 37.5 },
-{ hour: 0, minute: 0 }],
-[// 5
-{ hour: 6, minute: 15 },
-{ hour: 9, minute: 45 },
-{ hour: 0, minute: 15 },
-{ hour: 6, minute: 45 },
-{ hour: 3, minute: 15 },
-{ hour: 0, minute: 45 }],
-[// 6
-{ hour: 6, minute: 15 },
-{ hour: 9, minute: 45 },
-{ hour: 6, minute: 0 },
-{ hour: 6, minute: 45 },
-{ hour: 0, minute: 15 },
-{ hour: 0, minute: 45 }],
-[// 7
-{ hour: 3, minute: 15 },
-{ hour: 6, minute: 45 },
-{ hour: 7.5, minute: 37.5 },
-{ hour: 6, minute: 0 },
-{ hour: 7.5, minute: 37.5 },
-{ hour: 0, minute: 0 }],
-[// 8
-{ hour: 6, minute: 15 },
-{ hour: 6, minute: 45 },
-{ hour: 0, minute: 15 },
-{ hour: 0, minute: 45 },
-{ hour: 0, minute: 15 },
-{ hour: 0, minute: 45 }],
-[// 9
-{ hour: 6, minute: 15 },
-{ hour: 6, minute: 45 },
-{ hour: 3, minute: 0 },
-{ hour: 6, minute: 0 },
-{ hour: 3, minute: 15 },
-{ hour: 0, minute: 45 }]];
-
-
+  [
+    // 0
+    { hour: 6, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 0 },
+    { hour: 0, minute: 30 },
+    { hour: 3, minute: 0 },
+    { hour: 0, minute: 45 },
+  ],
+  [
+    // 1
+    { hour: 7.5, minute: 37.5 },
+    { hour: 6, minute: 30 },
+    { hour: 7.5, minute: 37.5 },
+    { hour: 6, minute: 0 },
+    { hour: 7.5, minute: 37.5 },
+    { hour: 0, minute: 0 },
+  ],
+  [
+    // 2
+    { hour: 3, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 15 },
+    { hour: 0, minute: 45 },
+    { hour: 0, minute: 15 },
+    { hour: 9, minute: 45 },
+  ],
+  [
+    // 3
+    { hour: 3, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 3, minute: 15 },
+    { hour: 9, minute: 0 },
+    { hour: 3, minute: 15 },
+    { hour: 9, minute: 0 },
+  ],
+  [
+    // 4
+    { hour: 6, minute: 30 },
+    { hour: 6, minute: 30 },
+    { hour: 0, minute: 15 },
+    { hour: 6, minute: 0 },
+    { hour: 7.5, minute: 37.5 },
+    { hour: 0, minute: 0 },
+  ],
+  [
+    // 5
+    { hour: 6, minute: 15 },
+    { hour: 9, minute: 45 },
+    { hour: 0, minute: 15 },
+    { hour: 6, minute: 45 },
+    { hour: 3, minute: 15 },
+    { hour: 0, minute: 45 },
+  ],
+  [
+    // 6
+    { hour: 6, minute: 15 },
+    { hour: 9, minute: 45 },
+    { hour: 6, minute: 0 },
+    { hour: 6, minute: 45 },
+    { hour: 0, minute: 15 },
+    { hour: 0, minute: 45 },
+  ],
+  [
+    // 7
+    { hour: 3, minute: 15 },
+    { hour: 6, minute: 45 },
+    { hour: 7.5, minute: 37.5 },
+    { hour: 6, minute: 0 },
+    { hour: 7.5, minute: 37.5 },
+    { hour: 0, minute: 0 },
+  ],
+  [
+    // 8
+    { hour: 6, minute: 15 },
+    { hour: 6, minute: 45 },
+    { hour: 0, minute: 15 },
+    { hour: 0, minute: 45 },
+    { hour: 0, minute: 15 },
+    { hour: 0, minute: 45 },
+  ],
+  [
+    // 9
+    { hour: 6, minute: 15 },
+    { hour: 6, minute: 45 },
+    { hour: 3, minute: 0 },
+    { hour: 6, minute: 0 },
+    { hour: 3, minute: 15 },
+    { hour: 0, minute: 45 },
+  ],
+];
 
 const happyDigit = Array(6).fill({ hour: 22.5, minute: 7.5 });
 const neutralDigit = Array(6).fill({ hour: 7.5, minute: 7.5 });
@@ -83,15 +102,15 @@ function showSpecialState(digit) {
   window.setTimeout(() => startClock(), 10000);
 }
 
-document.querySelector('.trigger--left').addEventListener('click', e => {
+document.querySelector('.trigger--left').addEventListener('click', (e) => {
   showSpecialState(happyDigit);
 });
 
-document.querySelector('.trigger--right').addEventListener('click', e => {
+document.querySelector('.trigger--right').addEventListener('click', (e) => {
   showSpecialState(neutralDigit);
 });
 
-window.addEventListener('keydown', e => {
+window.addEventListener('keydown', (e) => {
   const h = 72;
   const n = 78;
   if (h === e.keyCode) {
@@ -102,7 +121,7 @@ window.addEventListener('keydown', e => {
   }
 });
 
-document.querySelector('.art').addEventListener('click', e => {
+document.querySelector('.art').addEventListener('click', (e) => {
   document.querySelector('.text').classList.toggle('s-hidden');
   document.querySelector('.art').classList.toggle('art--full');
 });
@@ -118,7 +137,10 @@ function minuteToDegrees(minute) {
 function setHands(id, hour, minute) {
   const clock = document.querySelector(`.clock--${id}`);
   clock.style.setProperty(`--small-hand`, `${hourToDegrees(hour) + 360}deg`);
-  clock.style.setProperty(`--large-hand`, `${minuteToDegrees(minute) - 360}deg`);
+  clock.style.setProperty(
+    `--large-hand`,
+    `${minuteToDegrees(minute) - 360}deg`
+  );
 }
 
 function setDigit(id, values) {
